7e1875f54b5c2b9116214a91ae209799ff93c5c4
Othmane901 <bengharbi12@gmail.com>
2022-12-02 06:45:40 -0800

    shell

---
+++ errors.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * errors - print errors
+ * Return: void
+ */
+void errors(int error)
+{
+  switch (error)
+    {
+    case 1: /* fork error (stderr)*/
+      write(STDERR_FILENO, ERR_FORK, _strlen(ERR_FORK));
+      perror("Error");
+      break;
+
+    case 2: /* execve error (stderr) */
+      perror("Error");
+      break;
+
+    case 3: /* malloc error (stderr) */
+    write(STDERR_FILENO, ERR_MALLOC, _strelen(ERR_MALLOC));
+    break;
+
+    case 4: /* empty path error (stderr) */
+      write(STDERR_FILENO, ERR_PATH, _strlen(ERR_PATH));
+      break;
+
+    default;
+    return;
+    }
+}
