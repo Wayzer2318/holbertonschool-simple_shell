3098790b807b9e51e4d7777a84e9b40546f620f2
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-12-02 22:39:54 +0100

    task 1

--- shell.c
+++ shell.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+int main(void)
+{
+	char *args[] = {NULL, NULL};
+	char *command = NULL;
+	ssize_t n_bytes = 0;
+	size_t command_len = 0;
+
+	while(1)
+	{
+		printf("$");
+		n_bytes = getline(&command, &command_len, stdin);
+		command[n_bytes - 1] = '\0';
+		execve(*args, args, NULL);
+	}
+	return(0);
+}
