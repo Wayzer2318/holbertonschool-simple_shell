1daedeb4933a8b1499c0fb6977e20b3a8a6e0dc7
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-12-06 13:46:18 +0100

    task 1

--- errors.c
+++ errors.c
@@ -2,30 +2,31 @@
 
 /**
  * errors - print errors
+ * errors: the errors
  * Return: void
  */
 void errors(int error)
 {
-  switch (error)
-    {
-    case 1: /* fork error (stderr)*/
-      write(STDERR_FILENO, ERR_FORK, _strlen(ERR_FORK));
-      perror("Error");
-      break;
+	switch (error)
+	{
+	case 1: /* fork error (stderr)*/
+		write(STDERR_FILENO, ERR_FORK, _strlen(ERR_FORK));
+		perror("Error");
+		break;
 
-    case 2: /* execve error (stderr) */
-      perror("Error");
-      break;
+	case 2: /* execve error (stderr) */
+		perror("Error");
+		break;
 
-    case 3: /* malloc error (stderr) */
-    write(STDERR_FILENO, ERR_MALLOC, _strelen(ERR_MALLOC));
-    break;
+	case 3: /* malloc error (stderr) */
+		write(STDERR_FILENO, ERR_MALLOC, _strelen(ERR_MALLOC));
+		break;
 
-    case 4: /* empty path error (stderr) */
-      write(STDERR_FILENO, ERR_PATH, _strlen(ERR_PATH));
-      break;
+	case 4: /* empty path error (stderr) */
+		write(STDERR_FILENO, ERR_PATH, _strlen(ERR_PATH));
+		break;
 
-    default;
-    return;
-    }
+	default;
+	return;
+	}
 }
