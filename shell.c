6d65290c8f1f8ba3d49fc0179ab8eeaee1c8bf4a
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-12-06 13:41:43 +0100

    task 1

--- shell.c
+++ shell.c
@@ -7,21 +7,22 @@ int main(void)
 	ssize_t n_bytes = 0;
 	size_t command_len = 0;
 
-	while(1)
+	while (1)
 	{
 		printf("$ ");
 		n_bytes = getline(&command, &command_len, stdin);
 		command[n_bytes - 1] = '\0';
-		
+
 		args[0] = command;
-		
+
 		if (fork() == 0)
 		{
 			execve(*args, args, NULL);
 			dprintf(STDERR_FILENO, "%s : command not found.\n", *args);
 			exit(1);
 		}
-		else wait(NULL);
+		else 
+			wait(NULL);
 	}
 	return(0);
 }
